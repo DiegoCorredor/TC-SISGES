d1907eb18333e41b551ac53527a5f2d262bc5ad6
Jenifer Andrea Gómez <jenifergomez017@gmail.com>
2023-09-07 20:45:04 -0500

    TS-21

--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -5,15 +5,15 @@ import Login from '../pages/login.jsx';
 import Booking from '../pages/booking.jsx';
 import Error404 from '../pages/error404.jsx';
 import Dashboard from '../pages/dashboard.jsx';
-// import AddelementNavigate from '../pages/addelement.jsx';
-// import Searchelement from '../pages/searchelement.jsx';
-// import Editelement from '../pages/editelement.jsx';
-// import Delelement from '../pages/delelement.jsx';
-// import AddbookingNavigate from '../pages/addbooking.jsx';
-// import Searchbooking from '../pages/searchbooking.jsx';
-// import Editbooking from '../pages/editbooking.jsx';
-// import Deletebooking from '../pages/deletebooking.jsx';
-// import NewBooking from '../pages/newbooking.jsx';
+import AddelementNavigate from '../pages/addelement.jsx';
+import Searchelement from '../pages/searchelement.jsx';
+import Editelement from '../pages/editelement.jsx';
+import Delelement from '../pages/delelement.jsx';
+import AddbookingNavigate from '../pages/addbooking.jsx';
+import Searchbooking from '../pages/searchbooking.jsx';
+import Editbooking from '../pages/editbooking.jsx';
+import Deletebooking from '../pages/deletebooking.jsx';
+import NewBooking from '../pages/newbooking.jsx';
 
 export default createBrowserRouter([
     {
@@ -37,40 +37,40 @@ export default createBrowserRouter([
         path: '/dashboard',
         element: <Dashboard/>
     },
-    // {
-    //     path:'/addelement',
-    //     element: <AddelementNavigate/>
-    // },
-    // {
-    //     path:'/searchelement',
-    //     element: <Searchelement/>
-    // },
-    // {
-    //     path:'/editelement',
-    //     element: <Editelement/>
-    // },
-    // {
-    //     path:'/delelement',
-    //     element: <Delelement/>
-    // },
-    // {
-    //     path:'/addbooking',
-    //     element: <AddbookingNavigate/>
-    // },
-    // {
-    //     path:'/searchbooking',
-    //     element: <Searchbooking/>
-    // },
-    // {
-    //     path:'/editbooking',
-    //     element: <Editbooking/>
-    // },
-    // {
-    //     path:'/deletebooking',
-    //     element: <Deletebooking/>
-    // },
-    // {
-    //     path:'/bookingNew',
-    //     element: <NewBooking/>
-    // }
+    {
+        path:'/addelement',
+        element: <AddelementNavigate/>
+    },
+    {
+        path:'/searchelement',
+        element: <Searchelement/>
+    },
+    {
+        path:'/editelement',
+        element: <Editelement/>
+    },
+    {
+        path:'/delelement',
+        element: <Delelement/>
+    },
+    {
+        path:'/addbooking',
+        element: <AddbookingNavigate/>
+    },
+    {
+        path:'/searchbooking',
+        element: <Searchbooking/>
+    },
+    {
+        path:'/editbooking',
+        element: <Editbooking/>
+    },
+    {
+        path:'/deletebooking',
+        element: <Deletebooking/>
+    },
+    {
+        path:'/bookingNew',
+        element: <NewBooking/>
+    }
 ]) 
